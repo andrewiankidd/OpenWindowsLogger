18f6cca5050fd4c5e6e689fa50678e422bb228ba
Andrew Kidd <contact@andrewiankidd.co.uk>
2016-04-01 15:15:36 +0100

    Initial Commit
    initial (working) commit

---
+++ OpenWindowsLogger/OWLmain.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace OpenWindowsLogger
+{
+    partial class OWLmain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(OWLmain));
+      this.txtLogPath = new System.Windows.Forms.TextBox();
+      this.btnEnableLogging = new System.Windows.Forms.Button();
+      this.SuspendLayout();
+      // 
+      // txtLogPath
+      // 
+      this.txtLogPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.txtLogPath.Enabled = false;
+      this.txtLogPath.Location = new System.Drawing.Point(13, 13);
+      this.txtLogPath.Name = "txtLogPath";
+      this.txtLogPath.Size = new System.Drawing.Size(727, 20);
+      this.txtLogPath.TabIndex = 0;
+      // 
+      // btnEnableLogging
+      // 
+      this.btnEnableLogging.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.btnEnableLogging.ForeColor = System.Drawing.Color.Red;
+      this.btnEnableLogging.Location = new System.Drawing.Point(13, 40);
+      this.btnEnableLogging.Name = "btnEnableLogging";
+      this.btnEnableLogging.Size = new System.Drawing.Size(727, 23);
+      this.btnEnableLogging.TabIndex = 2;
+      this.btnEnableLogging.Text = "Logging Disabled";
+      this.btnEnableLogging.UseVisualStyleBackColor = true;
+      this.btnEnableLogging.Click += new System.EventHandler(this.btnEnableLogging_Click);
+      // 
+      // OWLmain
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(752, 75);
+      this.Controls.Add(this.btnEnableLogging);
+      this.Controls.Add(this.txtLogPath);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "OWLmain";
+      this.Text = "OWL";
+      this.Load += new System.EventHandler(this.OWLmain_Load);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+        }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtLogPath;
+    private System.Windows.Forms.Button btnEnableLogging;
+  }
+}
+
---
+++ OpenWindowsLogger/OWLmain.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+
+namespace OpenWindowsLogger
+{
+  public partial class OWLmain : Form
+  {
+    public static bool logEnabled = false;
+    public static string logPath = Environment.CurrentDirectory + "\\"+ DateTime.Now.ToString("yyyyMMddHHmmss") + "output.txt";
+    StreamWriter logFilew = new StreamWriter(logPath);
+
+    public OWLmain()
+    {
+      InitializeComponent();
+    }
+
+    private void OWLmain_Load(object sender, EventArgs e)
+    {
+      txtLogPath.Text = logPath;
+      if (!File.Exists(logPath)) { File.CreateText(logPath); }
+    }
+    
+    private void logWindows()
+    {
+      new Thread(() =>
+      {
+        Thread.CurrentThread.IsBackground = true;
+        foreach (KeyValuePair<IntPtr, string> window in OpenWindowGetter.GetOpenWindows())
+        {
+          IntPtr handle = window.Key;
+          string title = window.Value;
+          rwlog("w", "[" + DateTime.Now + "]" + "/" + handle + "/" + title);
+        }
+        if (!logEnabled) { return; } else { logWindows(); }
+      }).Start();
+    }
+
+    private void btnEnableLogging_Click(object sender, EventArgs e)
+    {
+      if (logEnabled)
+      {
+        logEnabled = false;
+        btnEnableLogging.Text = "Logging Disabled";
+        btnEnableLogging.ForeColor = Color.Red;
+        rwlog("w", "Logging Disabled - " + DateTime.Now);
+      }
+      else if (!logEnabled)
+      {
+        logEnabled = true;
+        btnEnableLogging.Text = "Logging Enabled";
+        btnEnableLogging.ForeColor = Color.Green;
+        rwlog("w", "Logging Enabled - " + DateTime.Now);
+        logWindows();
+      }
+    }
+
+    private void rwlog(string rw, string contents)
+    {
+
+      if (rw == "w")
+      {
+        logFilew.WriteLine(contents);
+        logFilew.Flush();
+      }
+
+      if (rw == "r")
+      {
+        //scrapped feature, live log, will redo
+      }
+    }
+
+   
+  }
+}
